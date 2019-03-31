a839fd886f5abebb83982bbc61e856c5208797bb
Varad Kshirsagar <f20170141@hyderabad.bits-pilani.ac.in>
2019-03-31 17:30:03 +0530

    Update BooksAPI.py

--- BooksAPI.py
+++ BooksAPI.py
@@ -1,15 +1,22 @@
 import requests
 import json
 import urllib
+import cachetools
+import time
+from cachetools import cached,TTLCache
+cache = TTLCache(maxsize = 100, ttl = 300)
 
+@cached(cache)
 def search_by_title(query):
     link = "https://www.googleapis.com/books/v1/volumes?q="+query
     return generate_result(link)
 
+@cached(cache)
 def search_by_ISBN(query):
     link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+query
     return generate_result(link)
 
+@cached(cache)
 def search_by_author(query):
     link = "https://www.googleapis.com/books/v1/volumes?q=inauthor:"+query
     return generate_result(link)
@@ -20,6 +27,7 @@ def generate_result(link):
     for x in response["items"]:
         final_result += x["volumeInfo"]["title"] + " by "
         for i in x["volumeInfo"]["authors"]:
-            final_result += i + " "
+            final_result += i + ", "
         final_result += '\n\n'
     return final_result
+
