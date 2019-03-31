56648318662bc943f235cc9c9990c8940e5df88e
Varad Kshirsagar <f20170141@hyderabad.bits-pilani.ac.in>
2019-04-01 02:52:59 +0530

    Update BooksAPI.py

--- BooksAPI.py
+++ BooksAPI.py
@@ -2,30 +2,47 @@ import requests
 import json
 import urllib
 import cachetools
-import time
 from functools import lru_cache
 
-@lru_cache(maxsize = 100)
+base_link = "https://www.googleapis.com/books/v1/volumes?q="
+results_references = []
 def search_by_title(query):
-    link = "https://www.googleapis.com/books/v1/volumes?q="+query
+    link = base_link + query
     return generate_result(link)
 
-@lru_cache(maxsize = 100)
 def search_by_ISBN(query):
-    link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+query
+    link = base_link + "isbn:"+ query
     return generate_result(link)
+
+def search_by_author(query):
+    link = base_link + "inauthor:"+ query
+    return generate_result(link)
+
+def results_string(results_list):
+    aux_result = ""
+    for i in results_list:
+        aux_result += "Title: " + i.title + "\n"
+        author = ""
+        for x in i.authors:
+            author += x + " "
+        aux_result += "Author(s): " + author + "\n"
+        aux_result += "Preview Link: " + i.previewLink + "\n\n"
+    return aux_result
 
 @lru_cache(maxsize = 100)
-def search_by_author(query):
-    link = "https://www.googleapis.com/books/v1/volumes?q=inauthor:"+query
-    return generate_result(link)
-
 def generate_result(link):
     response = requests.get(link).json()
-    final_result = ''
     for x in response["items"]:
-        final_result += x["volumeInfo"]["title"] + " by "
-        for i in x["volumeInfo"]["authors"]:
-            final_result += i + ", "
-        final_result += '\n\n'
-    return final_result
+        title = x["volumeInfo"]["title"]
+        authors = x["volumeInfo"]["authors"]
+        previewLink = x["volumeInfo"]["previewLink"]
+        b = Book(title,authors,previewLink)
+        results_references.append(b)
+    s = results_string(results_references)
+    return s
+
+class Book:
+    def __init__(self,title,authors,previewLink):
+        self.title = title
+        self.authors = authors
+        self.previewLink = previewLink
