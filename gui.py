1dd086537f7bacdaf8d3e6f6f5c37162396ff7cc
Varad Kshirsagar <f20170141@hyderabad.bits-pilani.ac.in>
2019-04-01 02:53:21 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,37 +1,36 @@
 from tkinter import *
-from BooksAPI import search_by_title,search_by_ISBN,search_by_author
+from BooksAPI import search_by_title,search_by_ISBN,search_by_author,results_references
 import time
-
+import copy
 
 def get_result():
-    result.set("")
+    results_references.clear()
     start = time.time()
     query = entry_field.get()
+    res.set("")
+    aux_result = ""
     if var.get() == 1:
-        result.set(search_by_title(query))
+        res.set(search_by_title(query))
     if var.get() == 2:
-        result.set(search_by_author(query))
+        res.set(search_by_author(query))
     if var.get() == 3:
-        result.set(search_by_ISBN(query))
+        res.set(search_by_ISBN(query))
     end = time.time()
     time1.set(end-start)
 
-
 #COMPONENTS:
-main = Tk(screenName = None,baseName = None,className = 'Book Search',useTk = 1)
+main = Tk(screenName = None,baseName = None,className = "Book Search",useTk = 1)
 entry_field = Entry(main)
+time1 = IntVar()
 time_label = Label(main,textvariable = time1)
+var = IntVar()
 r1 = Radiobutton(main,text = "Title",variable = var,value = 1)
 r2 = Radiobutton(main,text = "Author",variable = var,value = 2)
 r3 = Radiobutton(main,text = "ISBN",variable = var,value = 3)
 search_button = Button(main,text = 'Search',activebackground = '#add8e6',command = get_result)
-result_label = Label(main,textvariable = result)
+res = StringVar()
+result_label = Label(main,textvariable = res,justify = LEFT)
 main.state('zoomed')
-
-#VARIABLES:
-time1 = IntVar()
-var = IntVar()
-result = StringVar()
 
 #PACKING:
 entry_field.pack()
@@ -39,8 +38,10 @@ r1.pack()
 r2.pack()
 r3.pack()
 search_button.pack()
-result_label.pack()
 time_label.pack()
+result_label.pack(side = LEFT,fill = BOTH)
+
+
 
 #MAINLOOP
 main.mainloop()
