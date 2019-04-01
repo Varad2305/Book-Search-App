63f2a865af98a7a2aaa21c7073e926d69cb2864e
Varad Kshirsagar <f20170141@hyderabad.bits-pilani.ac.in>
2019-04-01 10:54:09 +0530

    Update BooksAPI.py

--- BooksAPI.py
+++ BooksAPI.py
@@ -24,6 +24,7 @@ def convert_to_string(results_list):                        #Takes information f
     aux_result = ""
     for i in results_list:
         aux_result += "Title: " + i.title + "\n"
+        aux_result += "Subtitle: " + i.subtitle + "\n"
         author = ""
         for x in i.authors:
             author += x + " "
@@ -32,19 +33,22 @@ def convert_to_string(results_list):                        #Takes information f
     return aux_result
 
 @lru_cache(maxsize = 100)                                   #Decorator to implement Least Recently Used cache
-def generate_result(link):                                  #Uses the Google Books API to get data which then, uses to create objects. Stores references to these objects in 2 different lists mentioned above
+def generate_result(link):                                  #Uses the Google Book   s API to get data which then, uses to create objects. Stores references to these objects in 2 different lists mentioned above
     response = requests.get(link).json()
     for x in response["items"]:
         title = x["volumeInfo"]["title"]
+        subtitle = x["volumeInfo"]["title"]
         authors = x["volumeInfo"]["authors"]
         previewLink = x["volumeInfo"]["previewLink"]
-        b = Book(title,authors,previewLink)
+        b = Book(title,subtitle,authors,previewLink)
         results_references.append(b)
         master_references.append(b)
     return convert_to_string(results_references)
 
+
 class Book:
-    def __init__(self,title,authors,previewLink):
+    def __init__(self,title,subtitle,authors,previewLink):
         self.title = title
+        self.subtitle = subtitle
         self.authors = authors
         self.previewLink = previewLink
